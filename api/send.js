92b52b49f1e0f40919ef16cbb56fe1637ebdbff2
Amzar <amzarirfanrazali@gmail.com>
2026-05-19 23:51:21 +0800

    netlify switch

--- api/send.js
+++ api/send.js
@@ -1,15 +1,18 @@
-export default async function handler(req, res) {
-  
-  const text = "✅ Test message from my bot!";
+exports.handler = async function (event) {
+
+  const text = "✅ Test message from Netlify bot!";
 
   await fetch(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      chat_id: 344886216,   // your chat_id
+      chat_id: 344886216,
       text: text
     })
   });
 
-  res.status(200).json({ success: true });
-}
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true })
+  };
+};
